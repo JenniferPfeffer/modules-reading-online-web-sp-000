4c2c2ab03dbd247a416af61a5fc07b0945733c47
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-03-13 20:48:00 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,12 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  include Dance
- 
+  include Dance       
+  extend MetaDancing  
   attr_accessor :name
- 
   def initialize(name)
     @name = name
   end
-  extend MetaDancing
 end
